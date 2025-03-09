63ff61ab9f52db2ce2359e5b8edad5f4612b41f7
prasad_jayanarasimhan <pjayanarasimhan@concord.net>
2025-03-09 12:52:09 +0530

    added vue toast

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import Toast, { POSITION, type PluginOptions } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import './style.css';
 import App from './App.vue';
 import router from './router';
@@ -7,4 +9,14 @@ import router from './router';
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+
+const toastOptions: PluginOptions = {
+    position: POSITION.BOTTOM_RIGHT,
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnHover: true,
+};
+  
+app.use(Toast, toastOptions);
+
 app.mount('#app');
--- src/stores/wishlist/wishlistStore.ts
+++ src/stores/wishlist/wishlistStore.ts
@@ -1,12 +1,14 @@
 import { defineStore } from 'pinia';
 import { computed, ref, watch } from 'vue';
 import { useProductStore } from '../products/productStore';
+import { useToast } from 'vue-toastification';
 
 const loadWishlist = (): number[] => {
     const storedWishlist = localStorage.getItem('wishlist');
     return storedWishlist ? JSON.parse(storedWishlist) : [];
   };
 export const useWishlistStore = defineStore('wishlist', () => {
+    const toast = useToast();
     const wishlist = ref<number[]>(loadWishlist());
     const productStore = useProductStore();
     const wishlistProducts = computed(() =>
@@ -16,8 +18,10 @@ export const useWishlistStore = defineStore('wishlist', () => {
     const toggleWishlist = (id: number) => {
         if(isWishlisted(id)) {
             wishlist.value = wishlist.value.filter(x => x !== id);
+            toast.info('Removed from Wishlist 🗑️');
         } else {
             wishlist.value.push(id);
+            toast.success('Added to Wishlist ❤️');
         }
     };
       
